4e8843201a661400ffeb5728c0d6b541e8c8ab87
MarAmoros <mamoroslopez.cf@iesesteveterradas.cat>
2022-03-09 19:53:21 +0100

    login html

---
+++ www/js/index.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+
+
+
+
+}

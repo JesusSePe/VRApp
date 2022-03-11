43f0ecf58d017b4e3b684617805a376ec2d4655a
MarAmoros <mamoroslopez.cf@iesesteveterradas.cat>
2022-03-11 17:14:07 +0100

    insert url

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,32 @@
 
 document.addEventListener('deviceready', onDeviceReady, false);
 
+
+
+
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
 
-
-
+    function blah(){
+        console.log("Funciona");
+        var urlIn = $("#urlIn").val();
+        console.log(urlIn);
+        $.ajax({
+            method: "GET",
+            url: urlIn,
+            dataType: "json",   // necessitem això pq ens retorni un objecte JSON
+          }).done(function (msg) {
+            for(let item in msg.artists) {
+              console.log(msg.artists[item]);
+              // aquí caldría fer mes coses, of course...
+              // ...
+            };
+          }).fail(function () {
+              alert("ERROR");
+          });
+    }
+   $("#sendBut").on("click", blah);
 
 }
